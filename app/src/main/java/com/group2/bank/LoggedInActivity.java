2272c9bc6c8d98744e312b27cb418e8f4421486b
brapana <55059816+brapana@users.noreply.github.com>
2021-04-30 17:56:48 -0700

    Update amount regex to not allow leading zeros

--- app/src/main/java/com/group2/bank/LoggedInActivity.java
+++ app/src/main/java/com/group2/bank/LoggedInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -38,25 +42,29 @@ public class LoggedInActivity extends AppCompatActivity {
         logout.setOnClickListener(v -> startActivity(new Intent(LoggedInActivity.this, MainActivity.class)));
 
         final TextInputEditText amount = findViewById(R.id.amount);
-        amount.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(10, 2)});
+        amount.setFilters(new InputFilter[]{new DecimalDigitsInputFilter()});
+
+
+
+
     }
 
     /**
-     * FIX REGEX:
-     *  - Restricts the decimals before digit to be up to 10, but if a period isn't typed then user can go up to the 11th place
-     *  - Doesn't check for leading 0's for example: 0123.12
-     *
-     * Source: This class is taken from Azhar on Tutorialspoint.
-     *    URL: https://www.tutorialspoint.com/how-to-limit-decimal-places-in-android-edittext
+     *  Restricts input to decimal numbers (max of 2 decimal digits) without leading zeros
      */
     class DecimalDigitsInputFilter implements InputFilter {
         private Pattern mPattern;
-        DecimalDigitsInputFilter(int digitsBeforeZero, int digitsAfterZero) {
-            mPattern = Pattern.compile("[0-9]{0," + digitsBeforeZero + "}+((\\.[0-9]{0," + (digitsAfterZero - 1) + "})?)||(\\.)?");
+        DecimalDigitsInputFilter() {
+            mPattern = Pattern.compile("^(0|[1-9][0-9]*)(\\.[0-9]{0,2})?$");
         }
         @Override
         public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-            Matcher matcher = mPattern.matcher(dest);
+
+            String result = dest.subSequence(0, dstart) + source.toString()
+                    + dest.subSequence(dend, dest.length());
+
+            Matcher matcher = mPattern.matcher(result);
+
             if (!matcher.matches())
                 return "";
             return null;
